3b9d669dbfd70c01602f6d7a0adde33e32e64275
jhiekkap <jari.hiekkapelto@gmail.com>
2020-08-15 16:22:04 +0300

    eka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ClientComponent from "./ClientComponent";
 
 function App() {
+  const [loadClient, setLoadClient] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* LOAD OR UNLOAD THE CLIENT */}
+      <button onClick={() => setLoadClient(prevState => !prevState)}>
+        STOP CLIENT
+      </button>
+      {/* SOCKET IO CLIENT*/}
+      {loadClient ? <ClientComponent /> : null}
+    </>
   );
 }
 
