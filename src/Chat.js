0dac66870d71b645a9ea08396b153c6ec529f113
jhiekkap <jari.hiekkapelto@gmail.com>
2020-08-16 12:48:40 +0300

    vastaanottajat

--- src/Chat.js
+++ src/Chat.js
@@ -7,26 +7,28 @@ export default function Chat() {
     const [messages, setMessages] = useState([])
     const [sender, setSender] = useState('')
     const [receiver, setReceiver] = useState('')
+    const [recipients, setRecipients] = useState('')
 
 
 
     //console.log('MESSAGES', messages)
     //console.log('SENDER: ', sender)
     // console.log('RECEIVER: ', receiver)
+    console.log('RECIPIENTS: ', recipients)
 
     useEffect(() => {
 
         console.log('USE EFFECT')
-        const random = parseInt(Math.random() * 2)
+        /* const random = parseInt(Math.random() * 2)
         console.log('Random', random)
         const chatID = random === 0 ? 'a' : 'b'
         console.log('chatId', chatID)
         setReceiver(random === 1 ? 'a' : 'b')
         setSender(chatID)
- 
+ */
         const socket = socketIOClient(ENDPOINT, {
             query: {
-                chatID
+                chatID: sender
             }
         });
 
@@ -39,47 +41,83 @@ export default function Chat() {
         return () => socket.disconnect();
         //
 
-    }, []);
+    }, [sender, receiver]);
 
 
-    const handleSendMessage = () => {
+    const handleSendMessage = (e) => {
+        e.preventDefault()
         const socket = socketIOClient(ENDPOINT);
         socket.emit('send_message', {
-            content: message,
             receiverChatID: receiver,
-            senderChatID: sender
+            senderChatID: sender,
+            content: message,
+            recipients,
         })
-        setMessages(prevState => prevState.concat(sender + ': ' + message));
+        if (recipients !== 'ALL') {
+            setMessages(prevState => prevState.concat(sender + ': ' + message));
+        }
         setMessage('')
     }
 
     return (
-        <div>
-            <div>
-                LÄHETTÄJÄ
-                <input
-                    type='text'
-                    value={sender}
-                    onChange={(e) => setSender(e.target.value)}
-                />
-            </div>
-            <div>
-                VASTAANOTTAJA
-                <input
-                    type='text'
-                    value={receiver}
-                    onChange={(e) => setReceiver(e.target.value)}
-                />
-            </div>
-          VIESTI
-            <input
-                type='text'
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <div>
-                <button type='button' onClick={handleSendMessage}>PUSH ME</button>
-            </div>
+        <div style={{ padding: '5%' }}>
+            <form onSubmit={handleSendMessage}>
+                <div >
+                    <div>
+                        LÄHETTÄJÄ
+                    </div>
+                    <input
+                        type='text'
+                        value={sender}
+                        onChange={(e) => setSender(e.target.value)}
+                    />
+                </div>
+                <br />
+                <div>
+                    <div>
+                        VASTAANOTTAJA
+                    </div>
+                    <input
+                        type='text'
+                        value={receiver}
+                        onChange={(e) => setReceiver(e.target.value)}
+                    />
+                </div>
+                <br />
+                <div>
+                    <div>
+                        VIESTI
+                    </div>
+                    <input
+                        type='text'
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <div>
+                        <br />
+                        <button type='submit'>PUSH ME</button>
+                    </div>
+                    <br />
+                    {message && <div>
+                        <input
+                            type='checkbox'
+                            checked={recipients !== 'ALL'}
+                            onChange={() => setRecipients(recipients !== 'ALL' ? 'ALL' : '')}
+                            name='recipients'
+                        />
+                        <label>YKSITYINEN</label>
+                        <input
+                            type='checkbox'
+                            checked={recipients === 'ALL'}
+                            onChange={() => setRecipients(recipients === 'ALL' ? '' : 'ALL')}
+                            name='recipients'
+                        />
+                        <label>KAIKKI</label>
+                    </div>}
+                </div>
+
+            </form>
+            <br />
             <ul>
                 {messages.map((msg, i) => <li key={i}>{msg}</li>)}
             </ul>
